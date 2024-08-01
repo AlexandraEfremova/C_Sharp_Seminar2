1ac23c36cad26be6bf9608b5f33096ff8397194b
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-01 10:25:49 +0300

    seminar2 : Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 5. Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет
+// Пример:
+// 456 ⇒ 6
+// 7812 ⇒ 1
+// 91 ⇒ Третьей цифры нет
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    while (number > 999)
+        // /= - аналог number = number /10
+        number /= 10; 
+    
+    System.Console.WriteLine(number % 10);
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет!");
+}
