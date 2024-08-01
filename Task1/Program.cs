6f4b2276e041ea8546950e0c655711ff3c11122b
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-01 10:24:31 +0300

    seminar2 : Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа
+// Примеры:
+// a = 256 => 26
+// a = 891 => 81
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    // Ишем первую цифру
+    int leftDigit = number / 100;
+    // Ищем вторую циру
+    int rightDigit = number % 10;
+    int result = leftDigit * 10 + rightDigit;
+    // Склеиваем значения
+    // System.Console.WriteLine(leftDigit + "" + rightDigit);
+    System.Console.WriteLine(result);  
+}
+else
+{
+    Console.WriteLine("Число НЕ трёхзначное!!!");
+}
